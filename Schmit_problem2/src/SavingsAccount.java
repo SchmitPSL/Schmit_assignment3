f4be57ad562515c0433166054981d58b488c3422
SchmitPSL <schmitpsl@yahoo.com>
2020-02-23 13:27:18 -0500

    Working

--- Schmit_problem2/src/SavingsAccount.java
+++ Schmit_problem2/src/SavingsAccount.java
@@ -1,11 +1,34 @@
 /*
- * Create class SavingsAccount. Use a static variable annualInterestRate to store the annual interest rate for all account holders. Each object of the class contains a private instance variable savingsBalance indicating the amount the saver currently has on deposit. Provide method calculateMonthlyInterest to calculate the monthly interest by multiplying the savingsBalance by annualInterestRate divided by 12—this interest should be added to savingsBalance. Provide a static method modifyInterestRate that sets the annualInterestRate to a new value.
- */
-public class SavingsAccount {
+Create class SavingsAccount. 
+Use a static variable annualInterestRate to store the annual interest rate for all account holders. 
+Each object of the class contains a private instance variable savingsBalance indicating the amount the saver currently has on deposit. 
+Provide method calculateMonthlyInterest to calculate the monthly interest
+	by multiplying the savingsBalance by annualInterestRate divided by 12—this interest should be added to savingsBalance. 
+Provide a static method modifyInterestRate that sets the annualInterestRate to a new value.
+*/
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class SavingsAccount {
+	
+	public static double annualInterestRate;
+	private double savingsBalance; // NEED TO BE INSTANCE VARIABLE
+
+	public SavingsAccount(double balance) {
+		this.savingsBalance = balance;
+	}
+	
+	public double getSavingsAccount() {
+		return this.savingsBalance;
+	}
+	
+	public static void modifyInterestRate(double newInterest) {
+		annualInterestRate = newInterest;
+	}
+	
+	public void calculateMonthlyInterest() {
+		double interest =(annualInterestRate * this.savingsBalance) / 12;
+		this.savingsBalance += interest;
 
 	}
+
 
 }
--- Schmit_problem2/src/SavingsAccountTest.java
+++ Schmit_problem2/src/SavingsAccountTest.java
@@ -1,11 +1,28 @@
 /*
- * Write a program called SavingsAccountTest to test class SavingsAccount. Instantiate two SavingsAccount objects, saver1 and saver2, with balances of $2000.00 and $3000.00, respectively. Set annualInterestRate to 4%, then calculate the monthly interest for each of 12 months and print the monthly balances for both savers. Next, set the annualInterestRate to 5%, calculate the next month’s interest and print the new balances for both savers.
  */
 public class SavingsAccountTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SavingsAccount saver1 = new SavingsAccount(2000.00);
+		SavingsAccount saver2 = new SavingsAccount(3000.00);
+		
+		SavingsAccount.modifyInterestRate(0.04);
+		
+		System.out.println("\t\tSaver1\tSaver2");
+		for(int i = 0; i < 12; i ++) {
+			saver1.calculateMonthlyInterest();
+			saver2.calculateMonthlyInterest();
+			
+			System.out.println("Month " + (i + 1) + ": " + String.format("\t%.2f", saver1.getSavingsAccount()) + " " + String.format("%.2f", saver2.getSavingsAccount()));
+		}
+		SavingsAccount.modifyInterestRate(0.05);
+		System.out.println("\t\tSaver1\tSaver2");
+		for(int i = 0; i < 12; i ++) {
+			saver1.calculateMonthlyInterest();
+			saver2.calculateMonthlyInterest();
+			
+			System.out.println("Month " + (i + 1) + ": " + String.format("\t%.2f", saver1.getSavingsAccount()) + " " + String.format("%.2f", saver2.getSavingsAccount()));
+		}
+		
 	}
-
 }
